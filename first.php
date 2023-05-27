ddbd72f30c99396599a40a856b79621042023f5c
pragatirajput07 <126343892+pragatirajput07@users.noreply.github.com>
2023-05-27 16:44:16 +0530

    added index.html

---
+++ first.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <center>
+      <h2 style="color:#d7385e;">NOTES</h2>
+      <h3 style="color:blue;">click below to download</h3>
+      <h4>DSA</h4>
+      <a href="anchal front.pdf" target="_biet">Download Now</a>
+      <h4>COA</h4>
+      <a href="anchal front.pdf" target="_biet">Download Now</a> 
+      <h4>TECHNICAL COMMUNICATION</h4>
+      <a href="anchal front.pdf" target="_biet">Download Now</a>
+      <h4>COMPUTER SYSTEM SECURITY</h4>
+      <a href="anchal front.pdf" target="_biet">Download Now</a> 
+      <h4>DISCRETE STRUCTURE</h4>
+      <a href="anchal front.pdf" target="_biet">Download Now</a>
+      <h4>MATHS 4</h4>
+      <a href="anchal front.pdf" target="_biet">Download Now</a>
+      <h4>PYTHON</h4>
+      <a href="anchal front.pdf" target="_biet">Download Now</a> 
+      <h4>TOC</h4>
+      <a href="anchal front.pdf" target="_biet">Download Now</a>
+      <h4>MICROPROCESSOR</h4>
+      <a href="anchal front.pdf" target="_biet">Download Now</a> 
+      <h4>DIGITAL ELECTRONICS</h4>
+      <a href="anchal front.pdf" target="_biet">Download Now</a>
+    </center>
+      </body>
+</html>
